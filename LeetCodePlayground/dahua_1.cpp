41b24a3ffea720ec5db94167ee8592ff8a404725
HaoXiaoChui <276281119@qq.com>
2020-09-16 19:32:17 +0800

    dahua1

---
+++ LeetCodePlayground/dahua_1.cpp
@@ -0,0 +1,27 @@
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+string input = "xiao fu zhen ke ai";
+
+int GetLstWord(string & s) {
+	int len = s.size();
+	int count=0;
+	if (s[len - 1] == ' ')
+	{
+		return 1;
+	}
+	for (int i=len-1;i>=0;i--)
+	{
+		if (s[i] == ' ')
+			break;
+		else
+			count++;
+	}
+	return count;
+}
+
+int main() {
+	int res=GetLstWord(input);
+	return res;
+}
